21bcfca75e74c89c2eac590c937b2d2da487cebc
unknown <doloresiravedra@gmail.com>
2026-05-04 08:40:29 -0300

    KN

---
+++ TP3 7)/src/tp3/pkg7/TP37.java
@@ -0,0 +1,22 @@
+
+package tp3.pkg7;
+
+import java.util.Scanner;
+
+public class TP37 {
+
+    public static void main(String[] args) {
+   Scanner millas = new Scanner(System.in);
+        double m = 0;
+        do{
+            System.out.println("Ingrese las millas para convertirlas en kilometros (ingrese 0 para terminar)");
+            m = millas.nextInt();
+            if(m == 0){
+                break;
+            }
+            double km = m * 1.6093;
+            System.out.println("KM = " + km);
+        } while (m != 0);
+        System.out.println("Terminando programa.");
+    }
+}
