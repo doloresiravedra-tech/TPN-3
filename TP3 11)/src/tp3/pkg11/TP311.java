148b65cea1aa9e6ea7934949f521e49ba10bd671
unknown <doloresiravedra@gmail.com>
2026-05-04 08:47:17 -0300

    XD

---
+++ TP3 11)/src/tp3/pkg11/TP311.java
@@ -0,0 +1,13 @@
+
+package tp3.pkg11;
+
+public class TP311 {
+
+    public static void main(String[] args) {
+ System.out.println("Todos los numeros del 100 al 0 con decrementos de 23.");
+        for (int i = 100; i >= 0; i -= 23){
+            System.out.println(i);
+        }
+    }
+}
+    
